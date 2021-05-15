7f1956ff175a494bc04d2448a026e525bd6b1fab
Mira Sohn <tonton07@gmail.com>
2021-05-15 00:42:36 -0400

    config files

---
+++ config/config_single1.R
@@ -0,0 +1,29 @@
+my.anno.db <- "EnsDb"  # DB for AnnotationHub 
+my.species <- "Homo sapiens"   # Species of the input dataset
+
+
+featurecounts.path <- list(HISAT2="../hisat2_output/featurecounts.tsv",
+                           STAR="../star_output/featurecounts.tsv")  # Paths to featurecounts count matrices 
+
+
+
+sample.csv <- "../config/sample_single1.csv"   # Path to the sample table csv file
+
+alpha <- 0.1   # Set threshold FDR
+
+
+
+mLog <- c(-1, 1)  # Set minimum log2 fold change of interest 
+
+basemean.comparison.ylog <- T
+lfc.comparison.ylog <- F
+padj.comparison.ylog <- F
+
+
+
+geneid.species <- "ENSG"  # Ensembl code for gene id given by species (e.g. ENSG (human), ENSMUSG (mouse))
+
+num.dis.genes <- 100      # Number of discordant genes (in percent difference). If num.dis.genes = 100, 100 top- and 100-bottom genes are considered at discordance level between HISAT2 and STAR
+
+
+
